1f40294367531e7f2c15e4822f6e560f2cd44e68
Marcelo Fortes <marcelofortes306@gmail.com>
2020-11-24 16:21:38 -0300

    Update 7.date-time-manip.php

--- 7.date-time-manip.php
+++ 7.date-time-manip.php
@@ -8,5 +8,24 @@
 <body>
     <a href="index.php">Back</a>
     <h1>Date and Time Manipulation</h1>
+
+    <?php 
+        //DATE
+        
+        echo "<h2>Date</h2>";
+        $datenow = getdate();
+        echo "Today's Day: ". $datenow['mday']."<br/>";
+        echo "Today's Month: ". $datenow['mon']."<br/>";
+        echo "Today's Year: ". $datenow['year']."<br/>";
+        $dd = $datenow['mday'];
+        $mm = $datenow['mon'];
+        $yyyy = $datenow['year'];
+        echo "Complete Date: $dd/$mm/$yyyy<br/>";
+        echo "<br>";
+        echo "<hr>";
+        
+        //TIME
+        print date("d/m/y G.i:s<br />", time())."<br />";
+    ?>
 </body>
 </html>
